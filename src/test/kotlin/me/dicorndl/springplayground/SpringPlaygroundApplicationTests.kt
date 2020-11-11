c04ac6bfe964605db5347870883768bccca3123d
dicorndl <dicorndl@gmail.com>
2020-11-11 22:22:36 +0900

    첫 커밋

---
+++ src/test/kotlin/me/dicorndl/springplayground/SpringPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dicorndl.springplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
