e584868e58dc9353b4df48dcef805d834c03a5c6
dicorndl <dicorndl@gmail.com>
2020-11-12 11:40:42 +0900

    코틀린 컨트롤러

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,20 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    runtimeOnly("com.h2database:h2")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
--- src/main/kotlin/me/dicorndl/springplayground/SpringPlaygroundApplication.kt
+++ src/main/kotlin/me/dicorndl/springplayground/SpringPlaygroundApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringPlaygroundApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringPlaygroundApplication>(*args)
+  runApplication<SpringPlaygroundApplication>(*args)
 }
