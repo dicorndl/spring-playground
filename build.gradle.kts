def438197218603d728a6141657fd53fb58d548a
dicorndl <dicorndl@gmail.com>
2020-11-15 22:53:09 +0900

    jvm 버전 11로 올림

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "me.dicorndl"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
   compileOnly {
@@ -62,6 +62,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
   kotlinOptions {
     freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "1.8"
+    jvmTarget = "11"
   }
 }
